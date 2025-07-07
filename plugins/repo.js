4da4c7206439219616841ab1f3abfdd31aa3672c
Malvin King <malvintech2@gmail.com>
2025-07-07 16:32:47 +0200

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -40,7 +40,7 @@ lite({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363398430045533@newsletter',
+                newsletterJid: '120363402507750390@newsletter',
                 newsletterName: 'Malvin Tech',
                 serverMessageId: 143
             }
