517fefc1c0aa4f35dc771df966f063f4df691b7d
Malvin King <malvintech2@gmail.com>
2025-07-07 16:21:33 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -48,10 +48,10 @@ module.exports = {
     MENTION_REPLY: process.env.MENTION_REPLY || "false",
     // make true if want auto voice reply if someone mention you 
 
-    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/qumhu4.jpg",
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/lm4a0b.jpg",
     // add custom menu and mention reply image url
 
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/xshsmk",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/lm4a0b.jpg",
     // add img for alive msg
 
     LIVE_MSG: process.env.LIVE_MSG || 
