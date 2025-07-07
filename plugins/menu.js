447c2f51f7307917699c470a76d3910cbe4107b4
Malvin King <malvintech2@gmail.com>
2025-07-07 16:29:11 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -86,7 +86,7 @@ ${menu.fun || '│ (No commands found)'}
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363398430045533@newsletter',
+                        newsletterJid: '120363402507750390@newsletter',
                         newsletterName: 'Lite XD',
                         serverMessageId: 143
                     }
