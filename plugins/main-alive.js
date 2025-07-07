48688655f993c5ba04bb78da203df6518723afba
Malvin King <malvintech2@gmail.com>
2025-07-07 16:30:41 +0200

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -39,7 +39,7 @@ lite({
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363398430045533@newsletter',
+                    newsletterJid: '120363402507750390@newsletter',
                     newsletterName: 'Malvin Tech',
                     serverMessageId: 143
                 }
